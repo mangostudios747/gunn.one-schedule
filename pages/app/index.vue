7f871b78e5ea8a03718cb0280aab2c535b225347
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-01-04 11:30:00 -0800

    minor style fixes

--- pages/app/index.vue
+++ pages/app/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col mt-5 w-full">
+  <div class="flex flex-col mt-5 pb-10 w-full">
     <div class="header w-[70%] mx-auto px-4 flex flex-row mb-3 text-center">
         <button @click="previousDay"><left-icon class="w-8 h-8 [stroke-linecap:round] [stroke-width:2] text-white" /></button>
         <div class="grow">
@@ -17,7 +17,7 @@
       class="class-block z-0"
       :class="period.status==0?'scale-105  period-status-0':`period-status-${period.status}`"
     >
-    <div :style="`background-color:${period.color[500] || period.color}; width:${period.progress}%;`" class="h-full transition-all duration-500 absolute left-0 rounded-md top-0 z-10">
+    <div :style="`background-color:${period.color[600] || period.color}ee; width:${period.progress}%;`" class="h-full transition-all duration-500 absolute left-0 rounded-md top-0 z-10">
     </div>
 
 
@@ -76,7 +76,7 @@ export default {
 
 <style>
 .class-block {
-  @apply mx-auto relative shadow-xl w-[70%] my-2 backdrop-blur-sm py-4 px-6 rounded-md;
+  @apply mx-auto relative shadow-indigo-600/50 shadow-2xl w-[70%] my-2 backdrop-blur-sm py-4 px-6 rounded-md;
 }
 
 .class-block .title {
