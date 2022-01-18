36b3a1139f312d7fdce6610b2aeff3d3acf8428d
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-01-17 22:26:26 -0800

    use OLD mongodb driver

--- server-middleware/database/index.js
+++ server-middleware/database/index.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_URL;
-const mdb = MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })//.then((c)=>c.db("users"));
+const mdb = MongoClient.connect(uri,
+   //{ useNewUrlParser: true, useUnifiedTopology: true }
+   )//.then((c)=>c.db("users"));
 
 module.exports = {
   mdb
