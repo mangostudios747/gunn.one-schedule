90c699d6d33251627550dbbb24d67cf7a3d56b91
saumyasinghal747 <saumyasmathtutoring@gmail.com>
2022-01-21 23:15:59 +0000

    actual filtering

--- pages/app/people/clubs.vue
+++ pages/app/people/clubs.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="flex flex-auto flex-col">
     <div class="flex w-full flex-row pt-1 px-2 gap-1">
-
-      <div class="grow basis-0 text-center"><dow button class="-">M</dow></div>
-      <div class="grow basis-0 text-center"><dow button class="">T</dow></div>
-      <div class="grow basis-0 text-center"><dow button class="">W</dow></div>
-      <div class="grow basis-0 text-center"><dow button class="">Th</dow></div>
-      <div class="grow basis-0 text-center"><dow button class="">F</dow></div>
+      <div class="grow basis-0 text-center"><dow :active="days['Monday']" @click="days['Monday'] = !days['Monday']" button class="-">M</dow></div>
+      <div class="grow basis-0 text-center"><dow :active="days['Tuesday']" @click="days['Tuesday'] = !days['Tuesday']" button class="">T</dow></div>
+      <div class="grow basis-0 text-center"><dow :active="days['Wednesday']" @click="days['Wednesday'] = !days['Wednesday']" button class="">W</dow></div>
+      <div class="grow basis-0 text-center"><dow :active="days['Thursday']" @click="days['Thursday'] = !days['Thursday']" button class="">Th</dow></div>
+      <div class="grow basis-0 text-center"><dow :active="days['Friday']" @click="days['Friday'] = !days['Friday']" button class="">F</dow></div>
     </div>
     <div
       :class="$route.params.club ? ' hidden' : 'flex flex-auto'"
@@ -38,11 +37,12 @@ export default {
         return Object.keys(this.days).filter(k=>this.days[k]);
     },
     filteredClubs() {
-      if (!selectedDays) return this.clubs;
+      if (!this.selectedDays.length) return this.clubs;
       else {
-          return this.clubs.filter((club)=>{
-            return this.selectedDays.includes(club.day)
-          })
+          return Object.fromEntries(
+    Object.entries(this.clubs).filter(([key, value]) => this.selectedDays.includes(value.day)) )
+            
+          
       }
     },
   },
