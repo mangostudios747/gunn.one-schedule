832625a42e0d6623b29a6b6b57c622ce6d857cb2
saumyasinghal747 <saumyasmathtutoring@gmail.com>
2022-02-01 08:22:05 +0000

    intialize files

---
+++ pages/app/course-catalog.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="h-full md:pl-2 pl-5 pr-5 w-full">
+    <h1 class="text-white text-3xl leading-9 font-bold md:my-3">Course Catalog</h1>
+    <div
+          class="flex sticky top-0 p-1 space-x-1 box"
+          role="tablist"
+          aria-orientation="horizontal"
+        >
+          <tab  href="/app/course-catalog/browse" active="$route.fullPath.split('/')[3]==='browse'">
+            Browse
+          </tab>
+          <tab href="/app/course-catalog/my-courses" active="$route.fullPath.split('/')[3]==='my-courses'">
+            My Courses
+          </tab>
+        </div>
+         <nuxt-child />
+    </div>
+</template>
+
+<script>
+export default {
+  middleware({redirect, route}){
+      const oopRegex = /^\/app\/course-catalog\/?$/;
+      if (oopRegex.test(route.fullPath)){
+          redirect('/app/course-catalog/browse')
+      }
+  },
+}
+</script>
+
+<style>
+
+</style>
