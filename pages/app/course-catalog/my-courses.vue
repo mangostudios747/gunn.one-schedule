f0456c2edc42e0e6a76174a2efb684dbfbbefb51
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-02-07 20:04:36 -0800

    shorten homework to hw

--- pages/app/course-catalog/my-courses.vue
+++ pages/app/course-catalog/my-courses.vue
@@ -6,7 +6,7 @@
 
             <div class="text-white flex gap-2 flex-row"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 my-auto" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
-</svg> {{$store.getters['catalog/totalHW']}} minutes homework / week</div>
+</svg> {{$store.getters['catalog/totalHW']}} minutes hw / week</div>
 
       </div>
     
