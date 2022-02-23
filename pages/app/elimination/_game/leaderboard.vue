f2d1eed2a25bd86f2f5185d3b1440022d3917364
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-02-23 00:42:24 -0800

    leaderboard rendering

--- pages/app/elimination/_game/leaderboard.vue
+++ pages/app/elimination/_game/leaderboard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="box flex flex-col">
-    {{l}}
-    <div v-for="item of l">
-      {{item.type}}
+  <div class="box flex p-2 space-y-1 mt-2 text-white flex-col">
+
+    <div class="font-bold py-2 px-3 rounded-lg hover:bg-white/10" v-for="(item, idx) of l">
+      {{idx+1}} {{item.user.firstName}} {{item.user.lastName}}
     </div>
   </div>
 </template>
