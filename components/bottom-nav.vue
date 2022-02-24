369912aa78ad5e7f28fcceaa6281637d98c91736
SaumyaSinghal747 <saumyasmathtutoring@gmail.com>
2022-02-24 01:50:30 -0800

    add elimination to bottom nav

--- components/bottom-nav.vue
+++ components/bottom-nav.vue
@@ -16,10 +16,11 @@
       <cog-icon  class="option-icon" />
       <span class="option-text">Settings</span>
     </nuxt-link>
-    <button @click="$store.commit('setSidebar', true)" class="option">
-      <dots-icon  class="option-icon" />
-      <span class="option-text">More</span>
-    </button>
+    <nuxt-link active-class="active" to="/app/elimination"  class="option">
+      <svg xmlns="http://www.w3.org/2000/svg" class="option-icon" viewBox="0 0 24 24" fill="transparent" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><line x1="22" y1="12" x2="18" y2="12"/><line x1="6" y1="12" x2="2" y2="12"/><line x1="12" y1="6" x2="12" y2="2"/><line x1="12" y1="22" x2="12" y2="18"/></svg>
+
+      <span class="option-text">Elimination</span>
+    </nuxt-link>
     </div>
 </template>
 
--- pages/app/elimination/_game.vue
+++ pages/app/elimination/_game.vue
@@ -3,22 +3,27 @@
     <div v-if="$fetchState.pending">
       Loading...
     </div>
-    <div v-else>
-    <h1 class="page-title">{{ game.name }}</h1>
-    <div
-      class="flex sticky top-0 p-1 mt-3 space-x-1 box"
-      role="tablist"
-      aria-orientation="horizontal"
-    >
-      <tab :href="`/app/elimination/${$route.params.game}/feed`">
-        Kill Feed
-      </tab>
-      <tab exact :href="`/app/elimination/${$route.params.game}`"> Home </tab>
-      <tab :href="`/app/elimination/${$route.params.game}/leaderboard`">
-        Leaderboard
-      </tab>
-    </div>
-    <nuxt-child />
+    <div class="w-full h-full" v-else>
+      <h1 class="page-title">{{ game.name }}</h1>
+      <div v-if="game.start">
+        <div
+          class="flex sticky top-0 p-1 mt-3 space-x-1 box"
+          role="tablist"
+          aria-orientation="horizontal"
+        >
+          <tab :href="`/app/elimination/${$route.params.game}/feed`">
+            Kill Feed
+          </tab>
+          <tab exact :href="`/app/elimination/${$route.params.game}`"> Home</tab>
+          <tab :href="`/app/elimination/${$route.params.game}/leaderboard`">
+            Leaderboard
+          </tab>
+        </div>
+        <nuxt-child/>
+      </div>
+      <div class="w-full h-full flex text-white text-3xl font-bold text-center" v-else>
+        <span class="my-auto mx-auto">Waiting for game to start...</span>
+      </div>
     </div>
   </div>
 </template>
@@ -27,28 +32,28 @@
 export default {
   data: () => ({
     game: null,
-    socket:null,
+    socket: null,
   }),
-  mounted(){
+  mounted() {
     this.socket = this.$nuxtSocket({
-      extraHeaders:{
+      extraHeaders: {
         Authorization: `Bearer ${localStorage.getItem('g1.eliminationUser')}`
       }
     });
-    this.socket.on('userInfo', (e, f)=>{
+    this.socket.on('userInfo', (e, f) => {
       console.log(e, f)
     })
-    this.socket.on('eliminationUpdateSelf', (e, f)=>{
+    this.socket.on('eliminationUpdateSelf', (e, f) => {
       console.log(e, f)
     })
-    this.socket.on('eliminationKill', (e, f)=>{
+    this.socket.on('eliminationKill', (e, f) => {
       console.log(e, f)
     })
-    this.socket.on('gameUpdated', (e, f)=>{
+    this.socket.on('gameUpdated', (e, f) => {
       this.game.fetchGame()
     })
 
-    this.socket.on('gameCreated', (e, f)=>{
+    this.socket.on('gameCreated', (e, f) => {
       console.log(e, f)
     })
   },
